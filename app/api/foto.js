599ca7dba21109062032f1297f0ce4e75f5733f3
Matheus dos Santos <matheusmendes1@hotmail.com>
2020-02-05 14:50:11 -0300

    End Chapter 4

--- app/api/foto.js
+++ app/api/foto.js
@@ -1,13 +1,21 @@
 let api = {};
 
+let fotos = [
+    {_id: 1, titulo: 'Leão', url:'http://www.fundosanimais.com/Minis/leoes.jpg' },
+    {_id: 2, titulo: 'Leão 2', url:'http://www.fundosanimais.com/Minis/leoes.jpg' }
+];
+
 api.lista = (req, res) => {
-
-    let fotos = [
-        {_id: 1, titulo: 'Leão', url:'http://www.fundosanimais.com/Minis/leoes.jpg' },
-        {_id: 2, titulo: 'Leão 2', url:'http://www.fundosanimais.com/Minis/leoes.jpg' }
-    ];
-
     res.json(fotos);
 }
+
+api.buscaPorId = (req, res) => {
+
+    let foto = fotos.find((foto) => {
+        return foto._id = req.params.id;
+    });
+
+    res.json(foto);
+};
 
 module.exports = api;
--- app/routes/foto.js
+++ app/routes/foto.js
@@ -2,6 +2,9 @@ module.exports = (app) => {
     //Verbos: POST PUT GET DELETE
 
     const api = app.api.foto;
+
     app.get('/v1/fotos', api.lista);
 
+    /* rota generica do tipo get recebendo um coringa */
+    app.get("/v1/fotos/:id", api.buscaPorId);
 }
