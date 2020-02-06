6e66acb3fa12dc2b9422200ecef09f86ca371c89
Matheus Santos <matheusmendes1@hotmail.com>
2020-02-06 19:38:13 -0300

    End Chapter 11

--- app/api/foto.js
+++ app/api/foto.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 const api = {};
+/* Mesmo nome do model criado em foto.js de models */
+const model = mongoose.model('Foto');
 
 api.lista = (req, res) => {
-
-    /* Mesmo nome do model criado em foto.js de models */
-    let model = mongoose.model('Foto');
 
     model
         .find({})
@@ -19,21 +18,56 @@ api.lista = (req, res) => {
 
 api.buscaPorId = (req, res) => {
 
-    
+    model
+        .findById(req.params.id)
+        .then((foto) => {
+            if(!foto) throw Error('Foto não encontrada!');
+            res.json(foto);
+        }),
+        (error) => {
+            console.log(error);
+            res.status(500).json(error)
+        }
 };
 
 api.removePorId = (req, res) => {
 
-
+    model
+        .remove({_id: req.params.id})
+        .then( () => {
+            res.sendStatus(204);
+        }),
+        (error) => {
+            console.log(error);
+            res.status(500).json(error);
+        }
 };
 
 api.adiciona = (req, res) => {
 
+    model
+        .create(req.body)
+        .then( (foto) => {
+            res.json(foto);
+        }),
+        (error) => {
+            console.log(error);
+            res.status(500).json(error);
+        }
 };
 
 api.atualiza = (req, res) => {
 
+    model
+        .findByIdAndUpdate(req.params.id, req.body)
+        .then( (foto) => {
 
+            res.json(foto);
+        }),
+        (error) => {
+            console.log(error);
+            res.status(500).json(error);
+        }
 }
 
 module.exports = api;
